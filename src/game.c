0da8e8ed29453627cf1736a4fcf0f1cd6d5b52cf
Stefan Eng <eng_steff@hotmail.com>
2018-03-07 07:56:35 +0100

    Re-indent game.c.

--- src/game.c
+++ src/game.c
@@ -2,52 +2,52 @@
 
 int main(void)
 {
-    GLFWwindow * window = init_window(1000, 600, "2018 -- Game");
+  GLFWwindow * window = init_window(1000, 600, "2018 -- Game");
 
-    if (window == NULL) {
-      error("Initialization failed, aborting.\n");
-      return EXIT_FAILURE;
-    }
+  if (window == NULL) {
+    error("Initialization failed, aborting.\n");
+    return EXIT_FAILURE;
+  }
 
-    /* Initialize sub-systems. */
-    init_environment();
+  /* Initialize sub-systems. */
+  init_environment();
 
-    GLuint program_default = shader_program_create(
-        "src/shaders/default.vert",
-        "src/shaders/default.frag");
-    if (program_default == 0) {
-      error("Shader program compilation failed.\n");
-      return EXIT_FAILURE;
-    }
+  GLuint program_default = shader_program_create(
+      "src/shaders/default.vert",
+      "src/shaders/default.frag");
+  if (program_default == 0) {
+    error("Shader program compilation failed.\n");
+    return EXIT_FAILURE;
+  }
 
-    size_t num_floats = 0;
-    GLfloat * floats = file_read_floats("data/basic_triangle.txt",
-        &num_floats);
+  size_t num_floats = 0;
+  GLfloat * floats = file_read_floats("data/basic_triangle.txt",
+      &num_floats);
 
-    struct vao vao = buffer_create(floats, num_floats);
-    program_use(program_default);
-    program_bind_mat4fv(program_default, UNIFORM_NAME_MVP, &m4_mvp);
+  struct vao vao = buffer_create(floats, num_floats);
+  program_use(program_default);
+  program_bind_mat4fv(program_default, UNIFORM_NAME_MVP, &m4_mvp);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-      /* Set time-delta. */
-      clock_tick();
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+    /* Set time-delta. */
+    clock_tick();
 
-      /* Clear color. */
-      glClear(GL_COLOR_BUFFER_BIT);
+    /* Clear color. */
+    glClear(GL_COLOR_BUFFER_BIT);
 
-      /* Draw triangle. */
-      draw_arrays(GL_TRIANGLES, &vao);
+    /* Draw triangle. */
+    draw_arrays(GL_TRIANGLES, &vao);
 
-      /* Poll for and process events. */
-      event_queue_process();
+    /* Poll for and process events. */
+    event_queue_process();
 
-      /* Swap front and back buffers. */
-      glfwSwapBuffers(window);
-    }
+    /* Swap front and back buffers. */
+    glfwSwapBuffers(window);
+  }
 
-    free(floats);
-    glfwTerminate();
-    return EXIT_SUCCESS;
+  free(floats);
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
